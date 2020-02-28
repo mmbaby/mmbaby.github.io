905db69d3a63abde1356c38f5714572c51758f15
lee seung min <mm_baby@naver.com>
2020-02-28 22:24:24 +0900

    맆블리입니다.

--- 02.27/shoppingmall.js
+++ 02.27/shoppingmall.js
@@ -1,10 +1,41 @@
 $(function(){
-   $('.box1 a').mouseover(function(){
-      $('.box1 span').stop().addClass('on');
-       
-   }); 
+    $(window).scroll(function(){
+       var documentPosition=$(document).scrollTop();
+    if(documentPosition>580){
+        $('.box3').stop().addClass('fixed');
+    }else{
+        $('.box3').stop().removeClass('fixed');
+    } 
     
-    $('.box1 a').mouseleave(function(){
-      $('.box1 span').stop().removeClass('on');
-   });    
+    });
+    
+    //product
+    setInterval(function(){
+        slide();
+    },2000);
+   
+    var target_odd=$('.product li:odd');
+    var target_even = $('.product li:even');
+    var change = 0;
+     function slide(){
+        if(change===0){
+            target_even.removeClass("on");
+            target_odd.addClass("on");
+        }else{
+            target_odd.removeClass("on");
+            target_even.addClass("on");
+        }
+         
+         change = (change+1)%2;
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
