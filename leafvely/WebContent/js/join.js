81b8a2552439a1c9a43573bd146c6895d4a6fedb
lee seung min <mm_baby@naver.com>
2020-03-04 20:23:56 +0900

    회원가입 페이지 수정

--- leafvely/WebContent/js/join.js
+++ leafvely/WebContent/js/join.js
@@ -1,68 +1,113 @@
-$(function(){
-   var joinPosition=$('.certification').offset().top;
-    
-    $('html, body').stop().animate({
-        'scrollTop':(joinPosition-100)+'px'
-    },500); 
-        
-    
-    //우편주소 찾기
-    $('#sample6_execDaumPostcode').click(function(){
-                new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+$(function () {
+    var joinPosition = $('.certification').offset().top;
 
-                // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+    $('html, body').stop().animate({
+        'scrollTop': (joinPosition - 100) + 'px'
+    }, 500);
+
+
+    //우편주소 찾기
+    $('#sample6_execDaumPostcode').click(function () {
+        new daum.Postcode({
+            oncomplete: function (data) {
+
                 var addr = ''; // 주소 변수
                 var extraAddr = ''; // 참고항목 변수
 
-                //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                if (data.userSelectedType === 'R') {
                     addr = data.roadAddress;
-                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                } else {
                     addr = data.jibunAddress;
                 }
 
-                // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-                if(data.userSelectedType === 'R'){
-                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                    if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+
+                if (data.userSelectedType === 'R') {
+                    if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
                         extraAddr += data.bname;
                     }
-                    // 건물명이 있고, 공동주택일 경우 추가한다.
-                    if(data.buildingName !== '' && data.apartment === 'Y'){
+
+                    if (data.buildingName !== '' && data.apartment === 'Y') {
                         extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
                     }
-                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                    if(extraAddr !== ''){
+
+                    if (extraAddr !== '') {
                         extraAddr = ' (' + extraAddr + ')';
                     }
-                    // 조합된 참고항목을 해당 필드에 넣는다.
+
                     document.getElementById("sample6_extraAddress").value = extraAddr;
-                
+
                 } else {
                     document.getElementById("sample6_extraAddress").value = '';
                 }
 
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+
                 document.getElementById('sample6_postcode').value = data.zonecode;
                 document.getElementById("sample6_address").value = addr;
-                // 커서를 상세주소 필드로 이동한다.
+
                 document.getElementById("sample6_detailAddress").focus();
             }
         }).open();
     });
-    
+
     //전체동의
-    $('#allagree').click(function(){
-        if($('input[id=allagree]').prop('checked')){
-            $('.agree_check input[type=checkbox]').prop('checked',true);
-        }else{
-            $('.agree_check input[type=checkbox]').prop('checked',false);
+    $('#allagree').click(function () {
+        if ($('input[id=allagree]').prop('checked')) {
+            $('.agree_check input[type=checkbox]').prop('checked', true);
+        } else {
+            $('.agree_check input[type=checkbox]').prop('checked', false);
         }
-        
+
+    });
+
+    //엔터키 submit 막기
+    $('input').keydown(function () {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+        }
+    })
+
+    //비밀번호 일치
+    $('#same').hide();
+    $('#discord').hide();
+    $('input[type=password]').keyup(function () {
+        var pw = $('input[name=pw]').val();
+        var pwCheck = $('input[name=pwCheck]').val();
+        if (pw != '' && pwCheck != '') {
+            if (pw === pwCheck) {
+                $('#same').show();
+                $('#discord').hide();
+                $('input[type=submit]').removeAttr('disabled');
+            } else {
+                $('#same').hide();
+                $('#discord').show();
+            }
+        }
+    });
+    //비밀번호 틀릴때 제출 안되게
+    $('input[type=submit]').click(function(){
+        var pw = $('input[name=pw]').val();
+        var pwCheck = $('input[name=pwCheck]').val();
+       if(pw!=pwCheck){
+           $(this).attr('disabled','disabled');
+           alert('PW를 다시 한번 확인해주세요');
+       }
     });
     
+    //이메일값 넣기
+    $('select[name=emailSelect]').stop().change(function() {
+        if ($(this).val() == '1') {
+            $('#email_2').val('');
+        } else {
+            $('#email_2').val($(this).val());
+            $('#email_2').attr('readonly', true);
+        }
+
+    });
+
+
+
+
+
+
+
 });
