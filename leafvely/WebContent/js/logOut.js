9026aeec1bf78bc2bf15f45d98ee821fce33f12a
NamHyeon <kssrcn@pukyong.ac.kr>
2020-03-05 10:39:46 +0900

    로그아웃 구현중

---
+++ leafvely/WebContent/js/logOut.js
@@ -0,0 +1,4 @@
+$(function() {
+	var logoutChange = document.getElementById('logoutChange');
+	logoutChange.innerHTML = '<a href="logout"><i class="fas fa-unlink"></i><span>LOGOUT</span></a>';
+});
---
+++ leafvely/src/com/leafvely/controller/LogOutController.java
@@ -0,0 +1,46 @@
+package com.leafvely.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.leafvely.command.Command;
+import com.leafvely.command.LoginCommand;
+
+/**
+ * Servlet implementation class LogOutController
+ */
+@WebServlet("/logout")
+public class LogOutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request,response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.sendRedirect("shoppingmall.jsp");
+	}
+
+}
