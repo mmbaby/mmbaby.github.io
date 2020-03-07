2da021798db50cff4cafbf269992827d2ad54ee0
lee seung min <mm_baby@naver.com>
2020-03-07 22:22:28 +0900

    오늘 하루 힘들엉

--- designer_folder/js/detailpage.js
+++ designer_folder/js/detailpage.js
@@ -31,6 +31,18 @@ $(function () {
             $('#option2').val('');
         }
     });
+    //별점
+    
+//    var inputScore=$('.star_label_ul li');
+//    inputScore.click(function(){
+//       if($(this).val()!='0'){
+//           $('input[class=select_input]').val();
+//           //$('input[class=select_input]').css('background-image': 'url(img/star.png)');
+//       } else{
+//           $('input[class=select_input]').val();
+//       }
+//    });
+    
 
     //추천배너 사진
     var count = 0;
@@ -62,7 +74,77 @@ $(function () {
          let last_index = $('.recommend_item').children().eq(-1).attr("data-index");
          $('.item[data-index='+(last_index)+']').prependTo($('.recommend_item'));
     });
-
+    
+    //카드할부 버튼
+    $('.tag input[id=notax]').click(function(){
+       $('.tax:even').addClass('do');
+        $('.tax:odd').removeClass('do');
+    });
+     $('.tag input[id=addtax]').click(function(){
+       $('.tax:odd').addClass('do');
+        $('.tax:even').removeClass('do');
+    });
+    
+    $('.tag li').eq(0).click(function(){
+       $(this).addClass('pick');
+        $('.tag li').eq(-1).removeClass('pick');
+    });
+    $('.tag li').eq(-1).click(function(){
+       $(this).addClass('pick');
+        $('.tag li').eq(0).removeClass('pick');
+    });
+    
+    $('.price input[id=card]').click(function(){
+       if($('.card_info').is(':visible')){
+           //true
+           $('.card_info').fadeOut('fast');
+           
+       }else{
+           //false
+           $('.card_info').fadeIn('fast');
+       }
+    });
+    
+    //nav 이동
+    var tab_1_Position=$('.boxE').offset().top;
+    var tab_2_Position=$('.boxH').offset().top;
+    var tab_3_Position=$('.boxJ').offset().top;
+    var tab_4_Position=$('.boxL').offset().top;
+    
+    $('.nav li a').click(function(event){
+       event.preventDefault;
+      var targetNav=$(this).attr('class');
+        if(targetNav==='tab_1'){
+            $('html, body').animate({
+                'scrollTop':(tab_1_Position-50)+'px'
+            },500);
+        }else if(targetNav==='tab_2'){
+            $('html, body').animate({
+                'scrollTop':(tab_2_Position-50)+'px'
+            },500);
+        }else if(targetNav==='tab_3'){
+            $('html, body').animate({
+                'scrollTop':(tab_3_Position-50)+'px'
+            },500);
+        }else{
+            $('html, body').animate({
+                'scrollTop':(tab_4_Position-50)+'px'
+            },500);
+        }
+            
+        
+    });
+    
+    //star_lable
+    $('.down').click(function(){
+        if($('.star_label_ul').children().is(':visible')){
+            $('.star_label_ul').children().hide();
+        }else{
+           $('.star_label_ul').children().show(); 
+        }
+    });
+    
+    
 });
 
 
